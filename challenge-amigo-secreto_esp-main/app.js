6d3896a13fd3233100f6c89a1a2205cd00b593d0
ro.izunza <ro.izunza@gmail.com>
2025-08-11 15:35:13 -0600

    Agregar función para añadir amigos con validación y limpiar el input

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,2 +1,23 @@
-//Definición de variable global
-let nombreAmistades = [];
+// Definición de variable global array para almacenar el nombre de los amigos
+let amigos = [];
+
+// Función para agregar amigos
+function agregarAmigo() {
+    // Se captura el valor del input
+    let nombreInput = document.getElementById('amigo').value.trim();
+
+    // Validar que el campo no se encuentre vacío
+    if (nombreInput === '') {
+        alert("Por favor ingrese un nombre"); // Mensaje de campo inválido
+        return; // Salir de la función si el campo está vacío
+    }
+
+    // Agregar el nombre válido al array 'amigos'
+    amigos.push(nombreInput);
+
+    // Limpiar el campo de entrada para que quede vacío
+    document.getElementById('amigo').value = '';
+
+    // Mostrar en la consola la lista actualizada de amigos para verificar
+    console.log("Lista de amigos:", amigos);
+}
